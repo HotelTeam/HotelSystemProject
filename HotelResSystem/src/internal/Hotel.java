65f21e40906a18b8ba153ea529e9adde43b102a7
geosakel77 <mai1428@uom.edu.gr>
2014-01-13 17:41:58 +0200

    Beginning of the project

---
+++ HotelResSystem/src/internal/Hotel.java
@@ -0,0 +1,27 @@
+package internal;
+
+import java.util.Vector;
+
+public class Hotel {
+private Vector<Room> rooms;
+private Vector<Reservation> reservations;
+public void modifyRooom(Room room){
+	
+	
+	
+	
+}
+public void  deleteRoom(Room room) {
+
+}
+public void setOffer(double offer) {
+	
+}
+public Vector<Reservation> getReservations() {
+	return reservations;
+}
+public Vector<Room> getRooms() {
+	return rooms;
+}
+
+}
